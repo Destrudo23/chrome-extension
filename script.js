4df1b8746a51faf3da42032d49a9a81c37859bc8
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-02-20 22:17:53 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
 
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = ""
+  }
+)
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+   // listItems += "<li> <a href='http://" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a> </li>"
+   listItems += `<li>
+                  <a target='_blank' href='http://${myLeads[i]}'>
+                    ${myLeads[i]}
+                  </a>
+                </li>`
+  }
+ulEl.innerHTML = listItems
+}
